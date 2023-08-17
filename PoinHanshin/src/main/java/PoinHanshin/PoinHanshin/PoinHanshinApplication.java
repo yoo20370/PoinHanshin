7d963d8e919a9d4933d037e4aae5f01efcf8815e
museongchoi <97529960+museongchoi@users.noreply.github.com>
2023-08-17 15:45:28 +0900

    test

---
+++ PoinHanshin/src/main/java/PoinHanshin/PoinHanshin/PoinHanshinApplication.java
@@ -0,0 +1,13 @@
+package PoinHanshin.PoinHanshin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoinHanshinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoinHanshinApplication.class, args);
+	}
+
+}
---
+++ PoinHanshin/src/test/java/PoinHanshin/PoinHanshin/PoinHanshinApplicationTests.java
@@ -0,0 +1,13 @@
+package PoinHanshin.PoinHanshin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoinHanshinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
