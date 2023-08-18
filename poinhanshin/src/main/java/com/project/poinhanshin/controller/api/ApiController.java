82d9419e1c80cc804432ba5154af14c374a32460
yoo20370 <yoo20370@gmail.com>
2023-08-18 18:58:28 +0900

    Test1

--- poinhanshin/src/main/java/com/project/poinhanshin/controller/api/ApiController.java
+++ poinhanshin/src/main/java/com/project/poinhanshin/controller/api/ApiController.java
@@ -1,7 +1,16 @@
 package com.project.poinhanshin.controller.api;
 
+import com.project.poinhanshin.etc.ApiExplorer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ApiController {
+    ApiExplorer apiExplorer;
+    
+    @Autowired
+    public ApiController(ApiExplorer apiExplorer) {
+        this.apiExplorer = apiExplorer;
+    }
+
 }
--- poinhanshin/src/main/java/com/project/poinhanshin/etc/ApiExplorer.java
+++ poinhanshin/src/main/java/com/project/poinhanshin/etc/ApiExplorer.java
@@ -1,4 +1,7 @@
 package com.project.poinhanshin.etc;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ApiExplorer {
 }
