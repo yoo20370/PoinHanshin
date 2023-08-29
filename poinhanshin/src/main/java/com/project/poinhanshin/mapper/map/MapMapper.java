a000b58ac63538425497677839af64f72f5429d4
yoo20370 <yoo20370@gmail.com>
2023-08-29 18:31:42 +0900

    mapMapper.xml , mapMaper , mapMapperImpl 작성
    작성만 했을 뿐 제대로 동작하는지 테스트 하지 않음
    ** 카테고리 떄문에 DB 수정이 필요할 수 있음

--- poinhanshin/src/main/java/com/project/poinhanshin/mapper/map/MapMapper.java
+++ poinhanshin/src/main/java/com/project/poinhanshin/mapper/map/MapMapper.java
@@ -1,4 +1,33 @@
 package com.project.poinhanshin.mapper.map;
 
+import com.project.poinhanshin.domain.etc.SearchCondition1;
+import com.project.poinhanshin.domain.map.MapBoardDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface MapMapper {
+
+    // 검색된 맵 게시물 수를 가져온다.
+    int selectMapCnt(SearchCondition1 sc);
+
+    // 검색된 맵 게시물 리스트를 가져온다.
+    List<MapBoardDto> selectMapList(SearchCondition1 sc);
+
+    // 하나의 맵 게시물 정보를 가져온다.
+    MapBoardDto selectMapBoardOne(Integer mapboardno);
+
+    // 맵 게시물 등록
+    int insertMapBoard(MapBoardDto mapBoardDto);
+
+    // 맵 게시물 수정
+    int updateMapBoard(MapBoardDto mapBoardDto);
+
+    // 맵 게시물 삭제
+    int deleteMapBoard(Integer mapboard_userno , Integer mapboardno);
+
+
+
+
 }
