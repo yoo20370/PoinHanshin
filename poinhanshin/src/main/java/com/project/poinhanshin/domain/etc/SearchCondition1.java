39e8a782d8549f7256336d1f23a7473775bbce57
yoo20370 <yoo20370@gmail.com>
2023-09-05 12:06:33 +0900

    mapMapper.xml 수정
    mapboard_ani_category , writertype 필터 sql 추가 (아직 테스트 안 함)

--- poinhanshin/src/main/java/com/project/poinhanshin/domain/etc/SearchCondition1.java
+++ poinhanshin/src/main/java/com/project/poinhanshin/domain/etc/SearchCondition1.java
@@ -15,15 +15,22 @@ public class SearchCondition1 {
     private String option = "";
     // 0 - 모두 , 1 - 고양이, 2 - 강아지
     private Integer ani_category = 0;
+    // 사용자 타입 0 - 모두 , 1 - 신고자 , 2 - 발견자
+    private Integer userType = 0;
+    // 지역
+    // private String city = "";
 
     public SearchCondition1(){}
-    public SearchCondition1(Integer page, Integer pageSize, String keyword, String option , Integer ani_category) {
+
+    public SearchCondition1(Integer page, Integer pageSize, String keyword, String option, Integer ani_category, Integer userType) {
         this.page = page;
         this.pageSize = pageSize;
         this.keyword = keyword;
         this.option = option;
         this.ani_category = ani_category;
+        this.userType = userType;
     }
+
     public String getQueryString(){
         return getQueryString(page);
     }
@@ -34,6 +41,7 @@ public class SearchCondition1 {
                 .queryParam("keyword" , keyword)
                 .queryParam("option" , option)
                 .queryParam("ani_category" , ani_category)
+                .queryParam("userType" , userType)
                 .build().toString();
     }
     public int getOffset() {
