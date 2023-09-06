b41f6768392238b403092ff2d906b3aec518d2dc
museongchoi <97529960+museongchoi@users.noreply.github.com>
2023-09-06 19:29:17 +0900

    09/06_domain_map/LocationDto Del
    AreaofinterestDto, MapBoardDto 변경
    LocationDto Del

--- poinhanshin/src/main/java/com/project/poinhanshin/domain/map/MapBoardDto.java
+++ poinhanshin/src/main/java/com/project/poinhanshin/domain/map/MapBoardDto.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import lombok.ToString;
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.sql.Timestamp;
 
 
 @Getter
@@ -16,25 +17,26 @@ import java.sql.Date;
 public class MapBoardDto {
     private Integer mapboard_userno;    // FK, 회원번호
     private Integer mapboardno;         // PK, 게시물내용
+    private String mapboard_title;    // 게시물 제목
     private String mapboard_content;             // sqltype : Text, 게시물 내용
-    private byte[] mapboard_mapimagepath;          // sqltype : longblob -> int & byte, 게시물 이미지 경로
-    private java.sql.Date missingtime;  // sqltype : date, 실종&발견 시간
+    private byte[] mapboard_imagepath;          // sqltype : longblob -> int & byte, 게시물 이미지 경로
+    private java.sql.Timestamp missingtime;  // sqltype : date, 실종&발견 시간
     private String missingaddress;      // 실종&발견 장소
     private java.math.BigDecimal latitude;  // sqltype : decimal, 위도
     private java.math.BigDecimal longitude; // sqltype : decimal, 경도
-    private java.sql.Date mapboard_reg_date;     // sqltype : date, 글 작성 시간
+    private java.sql.Timestamp mapboard_reg_date;     // sqltype : date, 글 작성 시간
     private Integer mapboard_viewcount;          // 조회수
     private boolean mapboard_ani_category;       // 강아지 0, 고양이 1
     private boolean writertype;         // 신고자 0, 발견자 1
 
     public MapBoardDto(){}
 
-    public MapBoardDto(Integer mapboard_userno, Integer mapboardno, String mapboard_content, byte[] mapboard_mapimagepath, Date missingtime, String missingaddress, BigDecimal latitude, BigDecimal longitude, Date mapboard_reg_date, Integer mapboard_viewcount, boolean mapboard_ani_category, boolean writertype) {
-
+    public MapBoardDto(Integer mapboard_userno, Integer mapboardno, String mapboard_title, String mapboard_content, byte[] mapboard_imagepath, Timestamp missingtime, String missingaddress, BigDecimal latitude, BigDecimal longitude, Timestamp mapboard_reg_date, Integer mapboard_viewcount, boolean mapboard_ani_category, boolean writertype) {
         this.mapboard_userno = mapboard_userno;
         this.mapboardno = mapboardno;
+        this.mapboard_title = mapboard_title;
         this.mapboard_content = mapboard_content;
-        this.mapboard_mapimagepath = mapboard_mapimagepath;
+        this.mapboard_imagepath = mapboard_imagepath;
         this.missingtime = missingtime;
         this.missingaddress = missingaddress;
         this.latitude = latitude;
@@ -44,4 +46,6 @@ public class MapBoardDto {
         this.mapboard_ani_category = mapboard_ani_category;
         this.writertype = writertype;
     }
+
+
 }
