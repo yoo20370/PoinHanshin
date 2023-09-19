3204e5628b6fd6e225d8404844d658eceea64e13
yoo20370 <yoo20370@gmail.com>
2023-09-19 13:43:18 +0900

    중간 커밋
    중간 커밋

--- poinhanshin/src/main/java/com/project/poinhanshin/mapper/mbti/MBTIMapper.java
+++ poinhanshin/src/main/java/com/project/poinhanshin/mapper/mbti/MBTIMapper.java
@@ -1,11 +1,11 @@
 package com.project.poinhanshin.mapper.mbti;
 
-import com.project.poinhanshin.domain.mbti.MBTInameKind;
+import com.project.poinhanshin.domain.mbti.MBTINameKind;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MBTIMapper {
 
     // 품종 번호, 품종 이름, mbti 유형
-    MBTInameKind searchAband(Integer MBTI);
+    MBTINameKind searchAband(Integer MBTI);
 }
