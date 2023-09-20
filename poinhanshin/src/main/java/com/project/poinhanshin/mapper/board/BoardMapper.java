f2056df7404112a8691096f47730d39d32127fcf
hs_py <wodud3xks@hs.ac.kr>
2023-09-20 12:30:23 +0900

    mapper @Param 추가
    mapper.xml에서 num인식때문에 추가

--- poinhanshin/src/main/java/com/project/poinhanshin/mapper/board/BoardMapper.java
+++ poinhanshin/src/main/java/com/project/poinhanshin/mapper/board/BoardMapper.java
@@ -3,6 +3,7 @@ package com.project.poinhanshin.mapper.board;
 import com.project.poinhanshin.domain.board.BoardDto;
 import com.project.poinhanshin.domain.etc.SearchCondition;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.HashMap;
 import java.util.List;
@@ -41,5 +42,5 @@ public interface BoardMapper {
     List<BoardDto> selectViewCntTop();
 
     // 댓글 개수 업데이트
-    int updateCommentCnt(Integer boardno, Integer num);
+    int updateCommentCnt(@Param("boardno")Integer boardno, @Param("num")Integer num);
 }
